68abce84a2bca9247f8d81406d42c4966345e0f7
André Breia <andre.breia.22@gmail.com>
2021-06-22 09:38:36 +0200

    wip

--- src/Models/Video.php
+++ src/Models/Video.php
@@ -21,4 +21,47 @@ class Video extends Model
     {
         return $this->morphTo();
     }
+
+    /**
+     * Get the Readable Size of a Video file.
+     * @return string
+     */
+    public function getReadableSizeAttribute() : string
+    {
+        return $this->getHumanReadableSize($this->size);
+    }
+
+    public function getDurationForHumansAttribute()
+    {
+        return \Carbon\CarbonInterval::seconds($this->video_duration)->cascade()->forHumans();
+    }
+
+    /**
+     * @param int $sizeInBytes
+     * @return string Formatted Filesize, e.g. "113.24 MB".
+     */
+    protected function getHumanReadableSize(int $sizeInBytes) : string
+    {
+        if ($sizeInBytes >= 1073741824) {
+            return number_format($sizeInBytes / 1073741824, 2).' GB';
+        }
+        
+        if ($sizeInBytes >= 1048576) {
+            return number_format($sizeInBytes / 1048576, 2).' MB';
+        }
+        
+        if ($sizeInBytes >= 1024) {
+            return number_format($sizeInBytes / 1024, 2).' KB';
+        }
+        
+        if ($sizeInBytes > 1) {
+            return $sizeInBytes.' bytes';
+        }
+        
+        if ($sizeInBytes == 1) {
+            return '1 byte';
+        }
+       
+        return '0 bytes';
+    }
 }
