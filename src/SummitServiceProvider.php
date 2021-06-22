d212e3dee3f0bec0e6283eee5e5e4d2f956027e2
Duncan McClean <duncan@steadfastcollective.com>
2021-06-22 11:39:24 +0100

    wip

--- src/SummitServiceProvider.php
+++ src/SummitServiceProvider.php
@@ -48,7 +48,7 @@ class SummitServiceProvider extends ServiceProvider
                     __DIR__.'/../database/migrations/create_course_blocks_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_course_blocks_table.php'),
                 ], 'migrations');
             }
-        
+
             if (! class_exists('CreateCourseBlockUserTable')) {
                 $this->publishes([
                     __DIR__.'/../database/migrations/create_course_block_user_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_course_block_user_table.php'),
