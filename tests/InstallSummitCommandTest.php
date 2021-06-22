f74386485424c2d0ed49a6e4bc8898dcaeedd8a1
Duncan McClean <duncan@steadfastcollective.com>
2021-06-22 16:35:09 +0100

    wip

--- tests/InstallSummitCommandTest.php
+++ tests/InstallSummitCommandTest.php
@@ -3,22 +3,48 @@
 namespace SteadfastCollective\Summit\Tests;
 
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\File;
+use Spatie\TestTime\TestTime;
 
 class InstallSummitCommandTest extends TestCase
 {
     /** @test */
     public function can_publish_config()
     {
+        $this->cleanup();
+
         $this->artisan('summit:install')
             ->expectsConfirmation("Publish config?", 'yes')
             ->expectsConfirmation("Publish migrations?", 'no');
             // ->expectsConfirmation("Publish Nova Resources?", 'no');
+
+        $this->assertFileExists(config_path('summit.php'));
     }
 
     /** @test */
     public function can_publish_migrations()
     {
-        //
+        TestTime::freeze();
+
+        $this->cleanup();
+
+        $this->artisan('summit:install')
+            ->expectsConfirmation("Publish config?", 'no')
+            ->expectsConfirmation("Publish migrations?", 'yes');
+            // ->expectsConfirmation("Publish Nova Resources?", 'no');
+
+        $datePrefix = date('Y_m_d_His', time());
+
+        $migrations = collect(File::allFiles(database_path('migrations')))
+            ->map(function ($file) {
+                return $file->getFilename();
+            })
+            ->toArray();
+
+        $this->assertTrue(in_array("{$datePrefix}_create_course_block_user_table.php", $migrations));
+        $this->assertTrue(in_array("{$datePrefix}_create_course_blocks_table.php", $migrations));
+        $this->assertTrue(in_array("{$datePrefix}_create_courses_table.php", $migrations));
+        $this->assertTrue(in_array("{$datePrefix}_create_videos_table.php", $migrations));
     }
 
     /** @test */
@@ -31,5 +57,21 @@ class InstallSummitCommandTest extends TestCase
     public function can_publish_everything()
     {
         //
+    }
+
+    protected function cleanup()
+    {
+        // Cleanup configs
+        if (File::exists(config_path('summit.php'))) {
+            File::delete(config_path('summit.php'));
+        }
+
+        // Cleanup migrations
+        collect(File::allFiles(database_path('migrations')))
+            ->each(function (\SplFileInfo $file) {
+                File::delete($file->getPathname());
+            });
+
+        // Cleanup Nova stuff... TODO
     }
 }
