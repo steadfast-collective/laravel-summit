71870990a154d9eed7304496801079574eea213c
Duncan McClean <duncan@steadfastcollective.com>
2021-06-23 10:01:24 +0100

    wip

---
+++ tests/Models/VideoTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace SteadfastCollective\Summit\Tests\Models;
+
+use SteadfastCollective\Summit\Tests\TestCase;
+
+class VideoTest extends TestCase
+{
+    /** @test */
+    public function can_get_readable_size_where_size_is_in_gb()
+    {
+        //
+    }
+
+    /** @test */
+    public function can_get_readable_size_where_size_is_in_mb()
+    {
+        //
+    }
+
+    /** @test */
+    public function can_get_readable_size_where_size_is_in_kb()
+    {
+        //
+    }
+
+    /** @test */
+    public function can_get_readable_size_where_size_is_in_bytes()
+    {
+        //
+    }
+
+    /** @test */
+    public function can_get_readable_size_where_size_is_1_byte()
+    {
+        //
+    }
+
+    /** @test */
+    public function can_get_readable_size_where_size_is_0_bytes()
+    {
+        //
+    }
+
+    /** @test */
+    public function can_get_duration_for_humans_where_length_is_in_seconds()
+    {
+
+    }
+
+    /** @test */
+    public function can_get_duration_for_humans_where_length_is_in_minutes()
+    {
+
+    }
+
+    /** @test */
+    public function can_get_duration_for_humans_where_length_is_in_hours()
+    {
+
+    }
+}
