411b7f4bac914eb923ed128613c993fcb90e864d
Duncan McClean <duncan@steadfastcollective.com>
2021-06-23 10:07:23 +0100

    wip

--- tests/Models/VideoTest.php
+++ tests/Models/VideoTest.php
@@ -2,44 +2,72 @@
 
 namespace SteadfastCollective\Summit\Tests\Models;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use SteadfastCollective\Summit\Models\Video;
 use SteadfastCollective\Summit\Tests\TestCase;
 
 class VideoTest extends TestCase
 {
+    use RefreshDatabase;
+
     /** @test */
-    public function can_get_readable_size_where_size_is_in_gb()
+    public function can_get_readable_size_where_size_is_in_gigabytes()
     {
-        //
+        $video = Video::create([
+            'size' => 2500000000,
+        ]);
+
+        $this->assertSame($video->readable_size, '2.33 GB');
     }
 
     /** @test */
-    public function can_get_readable_size_where_size_is_in_mb()
+    public function can_get_readable_size_where_size_is_in_megabytes()
     {
-        //
+        $video = Video::create([
+            'size' => 16000000,
+        ]);
+
+        $this->assertSame($video->readable_size, '15.26 MB');
     }
 
     /** @test */
-    public function can_get_readable_size_where_size_is_in_kb()
+    public function can_get_readable_size_where_size_is_in_kilobytes()
     {
-        //
+        $video = Video::create([
+            'size' => 2000,
+        ]);
+
+        $this->assertSame($video->readable_size, '1.95 KB');
     }
 
     /** @test */
     public function can_get_readable_size_where_size_is_in_bytes()
     {
-        //
+        $video = Video::create([
+            'size' => 57,
+        ]);
+
+        $this->assertSame($video->readable_size, '57 bytes');
     }
 
     /** @test */
     public function can_get_readable_size_where_size_is_1_byte()
     {
-        //
+        $video = Video::create([
+            'size' => 1,
+        ]);
+
+        $this->assertSame($video->readable_size, '1 byte');
     }
 
     /** @test */
     public function can_get_readable_size_where_size_is_0_bytes()
     {
-        //
+        $video = Video::create([
+            'size' => 0,
+        ]);
+
+        $this->assertSame($video->readable_size, '0 bytes');
     }
 
     /** @test */
