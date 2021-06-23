1585e4226387744ad69e8cc1aadb6a2e760b3731
Duncan McClean <duncan@steadfastcollective.com>
2021-06-23 10:36:12 +0100

    wip

---
+++ tests/Models/CourseBlockTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace SteadfastCollective\Summit\Tests\Models;
+
+use SteadfastCollective\Summit\Tests\TestCase;
+
+class CourseBlockTest extends TestCase
+{
+    /** @test */
+    public function couse_block_belongs_to_course()
+    {
+        //
+    }
+
+    /** @test */
+    public function course_block_belongs_to_many_users()
+    {
+        //
+    }
+
+    /** @test */
+    public function can_scope_to_available_from()
+    {
+        //
+    }
+
+    /** @test */
+    public function course_block_has_many_videos()
+    {
+        //
+    }
+
+    /** @test */
+    public function can_attach_video_to_course_block()
+    {
+        //
+    }
+
+    /** @test */
+    public function can_detach_video_from_course_block()
+    {
+        //
+    }
+
+    /** @test */
+    public function can_get_all_videos()
+    {
+        //
+    }
+
+    /** @test */
+    public function can_get_first_video()
+    {
+        //
+    }
+
+    /** @test */
+    public function can_get_last_video()
+    {
+        //
+    }
+}
