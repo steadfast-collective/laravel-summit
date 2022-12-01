9c0ec1b73a19a3115b87da6e591910948795298b
James Harley <27085725+JKHarley@users.noreply.github.com>
2022-12-01 09:20:25 +0000

    Update YouTubeDriver.php

--- src/VideoStorage/YouTubeDriver.php
+++ src/VideoStorage/YouTubeDriver.php
@@ -7,22 +7,12 @@ use SteadfastCollective\Summit\Contracts\VideoStorageDriver;
 use SteadfastCollective\Summit\Models\CourseBlock;
 use SteadfastCollective\Summit\Models\Video;
 
-class YouTubeDriver implements VideoStorageDriver
+class YouTubeDriver
 {
     const YOUTUBE_URL = 'https://www.youtube.com/watch?v=';
 
-    public function upload(CourseBlock $courseBlock, $file = null, $path = null, $type = null, $thirdPartyId = null): Video
+    public static function url(string $video): ?string
     {
-        return Video::create([
-            'third_party_id' => $thirdPartyId,
-            'provider' => 'youtube',
-        ]);
-    }
-
-    public function url(Video $video): ?string
-    {
-        $video = $video->third_party_id;
-
         return self::YOUTUBE_URL.$video;
     }
 }
